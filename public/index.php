b72129fb5d1337c653a71b0c611b7c4a51f76ddd
Kromian1 <mok1408@yandex.ru>
2026-04-07 22:45:27 +0300

    added index.phtml

--- public/index.php
+++ public/index.php
@@ -3,14 +3,29 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+session_start();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+$container = new Container();
+$container->set('renderer', function() {
+    return new PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function() {
+    return new \Slim\Flash\Messages();
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) use ($container) {
+    $messages = $this->get('flash')->getMessages();
+    $params = ['flash' => $messages];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
 });
 
 $app->run();

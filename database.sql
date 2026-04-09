41b82f3ac5ab1748f7120086f53879e089e347e5
Kromian1 <mok1408@yandex.ru>
2026-04-09 08:32:32 +0300

    added functions.php & added new controller

--- database.sql
+++ database.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id bigint primary key generated always as identity,
     name varchar(255) not null,
-    created_at timestamp
+    created_at timestamp default current_timestamp
 );
--- public/index.php
+++ public/index.php
@@ -5,10 +5,14 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 use DI\Container;
+use Db\Connection;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
+
+$conn = new Connection();
+$conn->get();
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -26,6 +30,19 @@ $app->get('/', function (Request $request, Response $response) use ($container) 
     $messages = $this->get('flash')->getMessages();
     $params = ['flash' => $messages];
     return $this->get('renderer')->render($response, 'index.phtml', $params);
+});
+
+$app->post('/urls', function (Request $request, Response $response) use ($container, $conn) {
+    $data = $request->getParsedBody();
+
+
+
+
+
+    $sql = "INSERT INTO urls (name) VALUES (:url)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':url', $data['url']);
+    $stmt->execute();
 });
 
 $app->run();

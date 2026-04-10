b3fcab093d070c6957bee1b9c34f551072c512f7
Kromian1 <mok1408@yandex.ru>
2026-04-10 08:19:34 +0300

    Добавлена таблица url_checks

--- database.sql
+++ database.sql
@@ -2,6 +2,16 @@ DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
     id bigint primary key generated always as identity,
-    name varchar(255) not null,
-    created_at timestamp default current_timestamp
+    name varchar(255) unique not null,
+    created_at timestamp default current_timestamp not null
+);
+
+CREATE TABLE url_checks (
+    id bigint primary key generated always as identity,
+    url_id bigint references urls(id) not null,
+    status_code int not null,
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
+    created_at timestamp default current_timestamp not null
 );

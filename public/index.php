be9b1aef074f5bdfe5012633753928ddd42e4eb7
Kromian1 <mok1408@yandex.ru>
2026-04-13 21:13:39 +0300

    fixes

--- public/index.php
+++ public/index.php
@@ -71,7 +71,14 @@ $app->post('/urls', function (Request $request, Response $response) use ($contai
 
     if ($error) {
         $container->get('flash')->addMessage('error', $error);
-        return $response->withHeader('Location', $router->urlFor('home'))->withStatus(302);
+
+        $messages = $container->get('flash')->getMessages();
+        $params = [
+            'flash' => $messages,
+            'title' => 'Анализатор страниц',
+            'router' => $router
+        ];
+        return $container->get('renderer')->render($response, 'index.phtml', $params)->withStatus(422);
     }
 
     $normalizedUrl = $container->get('urlValidator')->normalizeUrl($url);
